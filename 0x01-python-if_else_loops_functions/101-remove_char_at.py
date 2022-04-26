1700d1d0b866f97438acd04377740236c1f69659
Nachop51 <nachoperalta0@gmail.com>
2022-04-26 17:00:25 +0000

    Removing extra lines

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -5,10 +5,3 @@ def remove_char_at(str, n):
         if i != n:
             strcpy += str[i]
     return(strcpy)
-
-
-print(remove_char_at("Best School", 3))
-print(remove_char_at("Chicago", 2))
-print(remove_char_at("C is fun!", 0))
-print(remove_char_at("School", 10))
-print(remove_char_at("Python", -2))
