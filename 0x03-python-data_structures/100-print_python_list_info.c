c5ff698eee9833762f3fa0bc26d7c68caeedbf34
Nachop51 <nachoperalta0@gmail.com>
2022-05-02 22:56:01 +0000

    Uploaded from Ubuntu 14.04

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,16 @@
+#include <Python.h>
+#include <listobject.h>
+
+void print_python_list_info(PyObject *p)
+{
+	PyListObject *obj = (PyListObject *)p;
+	long int size = PyList_Size(p), i = 0;
+
+	printf("[*] Size of the Python List = %ld\n", size);
+	printf("[*] Allocated = %ld\n", obj->allocated);
+
+	for (i = 0; i < size; i++)
+	{
+		printf("Element %ld: %s\n", i, Py_TYPE(obj->ob_item[i])->tp_name);
+	}
+}
