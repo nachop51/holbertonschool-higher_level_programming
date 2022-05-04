b1a55ac6439abd96c19ef988fc60308e09c13ffa
Nachop51 <nachoperalta0@gmail.com>
2022-05-04 16:35:00 +0000

    Task 100, weighted average

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,3 +1,10 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
-    pass
+    if my_list == []:
+        return 0
+    sum = 0
+    div = 0
+    for tuple in my_list:
+        sum += tuple[0] * tuple[1]
+        div += tuple[1]
+    return (sum / div)
