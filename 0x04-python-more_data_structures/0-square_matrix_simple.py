36ee53537c93196a39a7fda540f940374d1b7335
Nachop51 <nachoperalta0@gmail.com>
2022-05-04 15:11:27 +0000

    Fixing task 0 and 1

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,18 +1,6 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    if matrix:
-        s_mtx = []
-        for row in matrix:
-            s_mtx.append(list(map(lambda x: x**2, row)))
-        return s_mtx
-
-
-matrix = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9]
-]
-
-new_matrix = square_matrix_simple(matrix)
-print(new_matrix)
-print(matrix)
+    s_mtx = []
+    for row in matrix:
+        s_mtx.append(list(map(lambda x: x**2, row)))
+    return s_mtx
