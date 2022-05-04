357b8e690054bea5e4cf6d2d56111b99e53a1919
Nachop51 <nachoperalta0@gmail.com>
2022-05-04 15:07:58 +0000

    Fixing task 7

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
-    if a_dictionary:
-        a_dictionary[key] = value
-        return a_dictionary
+    a_dictionary[key] = value
+    return a_dictionary
