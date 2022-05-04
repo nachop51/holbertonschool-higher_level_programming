640b21df95221e7a434cdfce73852d5f8888634c
Nachop51 <nachoperalta0@gmail.com>
2022-05-04 16:25:23 +0000

    Task Delete by value

---
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def complex_delete(a_dictionary, value):
+    delete = ""
+    for k, v in a_dictionary.items():
+        if v == value:
+            delete = k
+    if delete != "":
+        a_dictionary.pop(delete)
+    return a_dictionary
