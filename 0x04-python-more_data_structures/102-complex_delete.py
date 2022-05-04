1854527fcb36cde6ab73321d77373b9abbe7ef15
Nachop51 <nachoperalta0@gmail.com>
2022-05-04 16:26:09 +0000

    Fixing task 10

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 def complex_delete(a_dictionary, value):
-    delete = ""
-    for k, v in a_dictionary.items():
-        if v == value:
-            delete = k
-    if delete != "":
-        a_dictionary.pop(delete)
-    return a_dictionary
+    if a_dictionary:
+        delete = ""
+        for k, v in a_dictionary.items():
+            if v == value:
+                delete = k
+        if delete != "":
+            a_dictionary.pop(delete)
+        return a_dictionary
