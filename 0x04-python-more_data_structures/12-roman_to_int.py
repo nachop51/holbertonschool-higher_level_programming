100cb45b4d2db39e31ad332fb0aa0592bcf6e4ed
Nachop51 <nachoperalta0@gmail.com>
2022-05-04 15:03:58 +0000

    Fix variable name

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,43 +1,43 @@
 #!/usr/bin/python3
 def convert_nums(string):
-    l = []
+    list = []
     for i in string:
         if i == "M":
-            l.append(1000)
+            list.append(1000)
         elif i == "D":
-            l.append(500)
+            list.append(500)
         elif i == "C":
-            l.append(100)
+            list.append(100)
         elif i == "L":
-            l.append(50)
+            list.append(50)
         elif i == "X":
-            l.append(10)
+            list.append(10)
         elif i == "V":
-            l.append(5)
+            list.append(5)
         elif i == "I":
-            l.append(1)
+            list.append(1)
         else:
             return None
-    return l
+    return list
 
 
 def roman_to_int(roman_string):
     if roman_string and type(roman_string) is str:
-        l = convert_nums(roman_string)
-        if l is not None:
-            l.insert(0, 0)
-            for i in range(len(l)):
+        list = convert_nums(roman_string)
+        if list is not None:
+            list.insert(0, 0)
+            for i in range(len(list)):
                 if i == 0:
                     r = 0
                     c = 0
-                    v = l.pop()
+                    v = list.pop()
                     continue
                 if v >= c:
                     r += v
                 else:
                     r -= v
                 c = v
-                v = l.pop()
+                v = list.pop()
             return r
         else:
             return 0
