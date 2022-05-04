bb1748919ffd2116ca8bc78f56e45f1db07f8276
Nachop51 <nachoperalta0@gmail.com>
2022-05-04 16:21:07 +0000

    Fix: Extra parentheses

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_map(matrix=[]):
-    return [list(map(lambda i: list(map(lambda x: x**2, i)), matrix))]
+    return list(map(lambda i: list(map(lambda x: x**2, i)), matrix))
