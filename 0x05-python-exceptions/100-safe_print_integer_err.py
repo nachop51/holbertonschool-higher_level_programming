9cbc1383a09090da76ea42d4f8b172bba42dfea7
Nachop51 <nachoperalta0@gmail.com>
2022-05-16 15:37:27 +0000

    Removing extra text from print

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -2,7 +2,7 @@
 def safe_print_integer_err(value):
     import sys
     try:
-        print("value = {:d}".format(value))
+        print("{:d}".format(value))
         return True
     except Exception as e:
         print("Exception: {}".format(e), file=sys.stderr)
