8b7e080f921b795acaa6e3dc4a7ae22ffc8496ec
Nachop51 <nachoperalta0@gmail.com>
2022-05-16 15:27:04 +0000

    Fixing tasks

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -7,14 +7,3 @@ def safe_print_division(a, b):
     finally:
         print(f"Inside result: {c}")
         return c
-
-
-a = 12
-b = 2
-result = safe_print_division(a, b)
-print("{:d} / {:d} = {}".format(a, b, result))
-
-a = 12
-b = 0
-result = safe_print_division(a, b)
-print("{:d} / {:d} = {}".format(a, b, result))
