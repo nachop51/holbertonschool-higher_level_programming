5dcc4e9b3145ef153f7b7be7daf12342b5f29a57
Nachop51 <nachoperalta0@gmail.com>
2022-05-16 15:22:49 +0000

    test

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -4,7 +4,7 @@ def safe_print_list(my_list=[], x=0):
     while i < x:
         try:
             print(my_list[i], end="")
-        except:
+        except IndexError:
             break
         i += 1
     print()
--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -2,6 +2,6 @@
 def safe_print_integer(value):
     try:
         print("{:d}".format(value))
-    except:
+        return True
+    except TypeError:
         return False
-    return True
