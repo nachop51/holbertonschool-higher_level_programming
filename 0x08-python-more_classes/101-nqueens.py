0fa6d4b952bfd918b7da441b109152202f84a91e
Nachop51 <nachoperalta0@gmail.com>
2022-05-18 20:56:56 +0000

    Fixing comments, removing other solution

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -8,7 +8,7 @@ if len(sys.argv) != 2:
 
 try:
     BOARD_SIZE = int(sys.argv[1])
-except:
+except Exception:
     print("N must be a number")
     sys.exit(1)
 
@@ -17,33 +17,20 @@ if BOARD_SIZE < 4:
     sys.exit(1)
 
 
-def queens(n, i, a, b, c):
-    if i < n:
-        for j in range(n):
+def queens(size, i, a, b, c):
+    """Calculates the positions of the queens"""
+    if i < size:
+        for j in range(size):
             if j not in a and i + j not in b and i - j not in c:
-                yield from queens(n, i + 1, a + [j], b + [i + j], c + [i - j])
+                yield from queens(size, i + 1, a + [j],
+                                  b + [i + j], c + [i - j])
     else:
         yield a
 
 
 for solution in queens(BOARD_SIZE, 0, [], [], []):
-    print(list(enumerate(solution)))
-
-
-# def under_attack(col, queens):
-#     return col in queens or \
-#         any(abs(col - x) == len(queens)-i for i, x in enumerate(queens))
-
-
-# def solve(n):
-#     solutions = [[]]
-#     for row in range(n):
-#         solutions = [solution+[i]
-#                      for solution in solutions
-#                      for i in range(BOARD_SIZE)
-#                      if not under_attack(i, solution)]
-#     return solutions
-
-
-# for answer in solve(BOARD_SIZE):
-#     print(list(enumerate(answer)))
+    solve, i = [], 0
+    for sol in solution:
+        solve.append([i, sol])
+        i += 1
+    print(solve)
