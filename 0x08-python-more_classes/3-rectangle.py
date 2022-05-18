440f5cbe572d319c23ecbf7cf8d955714bd51009
Nachop51 <nachoperalta0@gmail.com>
2022-05-18 22:22:12 +0000

    Playing with one-line return statement

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -57,17 +57,3 @@ class Rectangle:
         #     for i in range(self.height):
         #         string += "#" * self.width + "\n"
         # return string[:-1]
-
-
-my_rectangle = Rectangle(2, 4)
-print("Area: {} - Perimeter: {}".format(my_rectangle.area(), my_rectangle.perimeter()))
-
-print(str(my_rectangle))
-print(repr(my_rectangle))
-
-print("--")
-
-my_rectangle.width = 10
-my_rectangle.height = 3
-print(my_rectangle)
-print(repr(my_rectangle))
