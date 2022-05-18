dd0cf0082244923810a95ee84e384dc00ad84c62
Nachop51 <nachoperalta0@gmail.com>
2022-05-18 22:20:59 +0000

    Playing with one-line return statement

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -54,6 +54,21 @@ class Rectangle:
         if self.height == 0 or self.width == 0:
             string += "\n"
         else:
-            for i in range(self.height):
-                string += "#" * self.width + "\n"
-        return string[:-1]
+            return (f"{'#'*self.width}\n"*self.__height).strip("\n")
+        #     for i in range(self.height):
+        #         string += "#" * self.width + "\n"
+        # return string[:-1]
+
+
+my_rectangle = Rectangle(2, 4)
+print("Area: {} - Perimeter: {}".format(my_rectangle.area(), my_rectangle.perimeter()))
+
+print(str(my_rectangle))
+print(repr(my_rectangle))
+
+print("--")
+
+my_rectangle.width = 10
+my_rectangle.height = 3
+print(my_rectangle)
+print(repr(my_rectangle))
