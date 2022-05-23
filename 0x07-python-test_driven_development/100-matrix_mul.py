ec965428532f54ecc11a5474f68dbda0e8138d86
Nachop51 <nachoperalta0@gmail.com>
2022-05-23 18:19:24 +0000

    Task 101 matrix multiplication

---
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+from multiprocessing.sharedctypes import Value
+
+
+def matrix_mul(m_a, m_b):
+    if not isinstance(m_a, list):
+        raise TypeError("m_a must be a list")
+    if not isinstance(m_b, list):
+        raise TypeError("m_b must be a list")
+    for array in m_a:
+        if not isinstance(array, list):
+            raise TypeError("m_a must be a list of lists")
+    for array in m_b:
+        if not isinstance(array, list):
+            raise TypeError("m_b must be a list of lists")
+    if m_a == [] or m_a == [[]]:
+        raise ValueError("m_a can't be empty")
+    if m_b == [] or m_b == [[]]:
+        raise ValueError("m_b can't be empty")
+    for array in m_a:
+        for num in array:
+            if not isinstance(num, int) and not isinstance(num, float):
+                raise TypeError("m_a should contain only integers or floats")
+    for array in m_b:
+        for num in array:
+            if not isinstance(num, int) and not isinstance(num, float):
+                raise TypeError("m_a should contain only integers or floats")
+    for array in m_a:
+        if len(array) != len(m_a[0]):
+            raise TypeError("each row of m_a must be of the same size")
+    for array in m_b:
+        if len(array) != len(m_b[0]):
+            raise TypeError("each row of m_a must be of the same size")
+    res = [[0 for x in range(len(m_b[0]))] for y in range(len(m_a))]
+    # For rows in m_a
+    for i in range(len(res)):
+        # For cols in m_b
+        for j in range(len(res[i])):
+            # For rows in m_b
+            for k in range(len(m_b)):
+                res[i][j] += m_a[i][k] * m_b[k][j]
+    return res
---
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+import numpy as np
+
+
+def lazy_matrix_mul(m_a, m_b):
+    return np.matmul(m_a, m_b)
