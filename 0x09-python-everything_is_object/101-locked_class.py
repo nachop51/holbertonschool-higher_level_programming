29286d70d541545adf25952b758ace1b82b92e5c
Nachop51 <nachoperalta0@gmail.com>
2022-05-24 23:15:12 +0000

    Tasks of the day

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
-class LockedClass:
+"""Locked class module"""
 
+
+class LockedClass:
+    """Locked Class"""
     __slots__ = ['first_name']
 
     def __init__(self, value=""):
+        """Inits the instance"""
         self.first_name = value
