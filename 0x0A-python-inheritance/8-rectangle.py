66ccb1391f0b8679b41d22f02d2b8f87d1ee663b
Nachop51 <nachoperalta0@gmail.com>
2022-05-30 22:18:47 +0000

    Changing definition of function by import

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -2,19 +2,7 @@
 ''' Module that creates a rectangle class '''
 
 
-class BaseGeometry:
-    ''' BaseGeometry class '''
-
-    def area(self):
-        ''' Area function (not implemented) '''
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        ''' Validates a value '''
-        if type(value) is not int:
-            raise TypeError(f"{name} must be an integer")
-        if value <= 0:
-            raise ValueError(f"{name} must be greater than 0")
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
