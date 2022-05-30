baf58c0db31b7875c5d9de43bfdf80fc1cdc495b
Nachop51 <nachoperalta0@gmail.com>
2022-05-30 22:22:54 +0000

    Changing definitions by imports

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -2,38 +2,9 @@
 ''' Module that creates a rectangle and a square class '''
 
 
-class BaseGeometry:
-    ''' BaseGeometry class '''
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
-    def area(self):
-        ''' Area function (not implemented) '''
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        ''' Validates a value '''
-        if type(value) is not int:
-            raise TypeError(f"{name} must be an integer")
-        if value <= 0:
-            raise ValueError(f"{name} must be greater than 0")
-
-
-class Rectangle(BaseGeometry):
-    ''' Rectangle class '''
-
-    def __init__(self, width, height):
-        ''' Initializes an instance of a rectangle '''
-        self.integer_validator("width", width)
-        self.integer_validator("height", height)
-        self.__width = width
-        self.__height = height
-
-    def area(self):
-        ''' Overrides the area method, and returns the area '''
-        return self.__width * self.__height
-
-    def __str__(self):
-        ''' Prints the dimensions of the rectangle '''
-        return f"[Rectangle] {self.__width}/{self.__height}"
+Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
