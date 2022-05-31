b1311eccd4edc7727fa2615d9cdf6ba3c8844874
Nachop51 <nachoperalta0@gmail.com>
2022-05-31 23:15:58 +0000

    Task 10

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -17,8 +17,8 @@ class Student:
         if attrs and isinstance(attrs, list):
             for attr in attrs:
                 if not isinstance(attr, str):
-                    return self.__dict__.copy()
+                    return self.__dict__
                 if attr in self.__dict__:
                     a_dict.update({attr: self.__dict__[attr]})
             return a_dict
-        return self.__dict__.copy()
+        return self.__dict__
