b5840bd0f194cc78542ebf1d7109400742b5a1ed
Nachop51 <nachoperalta0@gmail.com>
2022-05-31 22:33:25 +0000

    Fixing task 8

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-import json
 """ Class to json """
 
 
 def class_to_json(obj):
     """ Returns a json from an instance """
-    return json.dumps(obj.__dict__)
+    return obj.__dict__
