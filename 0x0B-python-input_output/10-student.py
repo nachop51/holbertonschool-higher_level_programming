94c6390814fb5b09e8e2bbcfac95f03469915d9b
Nachop51 <nachoperalta0@gmail.com>
2022-05-31 23:14:34 +0000

    Task 10

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -13,12 +13,11 @@ class Student:
 
     def to_json(self, attrs=None):
         """ Returns a json about the class """
+        a_dict = dict()
         if attrs and isinstance(attrs, list):
             for attr in attrs:
                 if not isinstance(attr, str):
                     return self.__dict__.copy()
-            a_dict = dict()
-            for attr in attrs:
                 if attr in self.__dict__:
                     a_dict.update({attr: self.__dict__[attr]})
             return a_dict
