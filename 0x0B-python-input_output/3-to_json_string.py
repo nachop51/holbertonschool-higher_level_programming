cfb039674a7ec3637742b6fdca97b3cbd0b852b7
Nachop51 <nachoperalta0@gmail.com>
2022-05-31 22:39:58 +0000

    Task 3

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 import json
-""" To json string module """
+""" json representation of a python object """
 
 
 def to_json_string(my_obj):
-    """ To json string """
+    """ To json string function, returns the object """
     return json.dumps(my_obj)
