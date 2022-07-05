23bf6791ea571beaf5ddbb82a4d0e9a7aa232fcb
Nachop51 <nachoperalta0@gmail.com>
2022-07-05 17:26:31 +0000

    Task 100

---
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -0,0 +1,2 @@
+-- Script that converts hbtn_0c_0 database to UTF8 (utf8mb4, collate utf8mb4_unicode_ci)
+ALTER TABLE hbtn_0c_0.first_table MODIFY name VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
