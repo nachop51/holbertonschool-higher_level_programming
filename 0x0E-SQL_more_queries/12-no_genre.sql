dc38062c798c477bcdfde06914712a84eb54173d
Nachop51 <nachoperalta0@gmail.com>
2022-07-06 20:09:26 +0000

    Fixing task 12

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -3,4 +3,4 @@
 -- Results must be sorted in ascending order by tv_shows.title and tv_show_genres.genre_id
 -- You can use only one SELECT statement
 -- The database name will be passed as an argument of the mysql command
-SELECT s.title, t.genre_id FROM tv_shows s LEFT JOIN tv_show_genres t ON s.id = t.genre_id WHERE t.genre_id IS NULL ORDER BY s.title ASC;
+SELECT s.title, t.genre_id FROM tv_shows s LEFT JOIN tv_show_genres t ON s.id = t.show_id WHERE t.genre_id IS NULL ORDER BY s.title, t.genre_id;
---
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,2 @@
+-- script that lists all genres from hbtn_0d_tvshows and displays the number of shows linked to each.
+SELECT FROM ;
