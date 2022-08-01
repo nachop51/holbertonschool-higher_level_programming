87842be9f5e87c9b2ce3351a77c20c9260a2d91b
Nachop51 <nachoperalta0@gmail.com>
2022-08-01 16:35:09 +0000

    Fixing semistandard and typos

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,6 +2,8 @@
 const process = require('process');
 if (process.argv.length === 2) {
   console.log('No argument');
-} else {
+} else if (process.argv.length === 3) {
   console.log('Argument found');
+} else {
+  console.log('Arguments found');
 }
--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const process = require('process');
-if (process.argv.length === 2) {
+if (!process.argv.slice(2)[0]) {
   console.log('No argument');
 } else {
   process.argv.slice(2).forEach((val) => {
