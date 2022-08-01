3c160bc9c3c6b410b6ec088957fe6eaeee1279ce
Nachop51 <nachoperalta0@gmail.com>
2022-08-01 23:33:35 +0000

    Typo fixed

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const array = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+const array = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 array.forEach((element) => {
   console.log(element);
 });
