30a640f0dfac47397538bda46e067155278fe4f4
Nachop51 <nachoperalta0@gmail.com>
2022-08-02 23:22:34 +0000

    Semistandard fixed

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const list = require("./100-data.js").list;
+const list = require('./100-data.js').list;
 const newList = list.map(function (item) {
   if (this.index === undefined) this.index = -1;
   this.index++;
