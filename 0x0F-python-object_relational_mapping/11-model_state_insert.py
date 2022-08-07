c34547d0bf05a30cace5a13eb0896ba81212c199
Nachop <nachop@Nachop.localdomain>
2022-08-06 21:22:03 -0300

    Task 11 and 12

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+    This module lists all state objects
+    that contains the letter a
+    from the databse hbtn_0e_6_usa
+"""
+if __name__ == '__main__':
+    import sys
+    from model_state import Base, State
+    from sqlalchemy import (create_engine)
+    from sqlalchemy.orm import Session
+
+    mysql_user = sys.argv[1]
+    mysql_pass = sys.argv[2]
+    mysql_db = sys.argv[3]
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+        mysql_user, mysql_pass, mysql_db), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    with Session(engine) as session:
+        state = State(name='Louisiana')
+        session.add(state)
+        session.commit()
---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+    This module lists all state objects
+    that contains the letter a
+    from the databse hbtn_0e_6_usa
+"""
+if __name__ == '__main__':
+    import sys
+    from model_state import Base, State
+    from sqlalchemy import create_engine, select
+    from sqlalchemy.orm import Session
+
+    mysql_user = sys.argv[1]
+    mysql_pass = sys.argv[2]
+    mysql_db = sys.argv[3]
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+        mysql_user, mysql_pass, mysql_db), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    with Session(engine) as session:
+        query = select(State).filter(State.id == 11)
+        result = session.execute(query).first()
+        if result[0] is not None:
+            result[0].name = 'New Mexico'
+            session.commit()
