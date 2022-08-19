47ce51734293da0a6dcfecab27497b741a7c910c
Nachop51 <nachoperalta0@gmail.com>
2022-08-19 00:14:16 +0000

    hotfix

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Bash script that sends a request to a URL passed as an argument, and displays only the status code of the response
-curl -LI "$1" -o /dev/null -w '%{http_code}\n' -s
+curl -I "$1" -o /dev/null -w '%{response_code}' -s
